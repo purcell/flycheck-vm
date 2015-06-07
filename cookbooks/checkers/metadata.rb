f64f093568e932bec180f571831548ec24301f3d
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-06-07 19:12:14 +0200

    Add missing apt dependency to checkers cookbook

--- cookbooks/checkers/metadata.rb
+++ cookbooks/checkers/metadata.rb
@@ -4,3 +4,5 @@ maintainer_email 'swiesner@lunaryorn.com'
 license          'X11/MIT'
 description      'Installs all syntax checkers supported by Flycheck'
 version          '0.1.0'
+
+depends 'apt'
