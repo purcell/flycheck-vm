60ff471dafa36aa7ddc071e431468a8ffc95f9b3
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-06-07 17:52:51 +0200

    Add a cookbook to install syntax checkers

--- Berksfile
+++ Berksfile
@@ -3,3 +3,4 @@ source "https://supermarket.chef.io"
 cookbook 'apt'
 cookbook 'locale'
 cookbook 'emacs', path: 'cookbooks/emacs'
+cookbook 'checkers', path: 'cookbooks/checkers'
---
+++ cookbooks/checkers/metadata.rb
@@ -0,0 +1,6 @@
+name             'checkers'
+maintainer       'Sebastian Wiesner'
+maintainer_email 'swiesner@lunaryorn.com'
+license          'X11/MIT'
+description      'Installs all syntax checkers supported by Flycheck'
+version          '0.1.0'
